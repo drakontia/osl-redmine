0a60aa8e32dee009a70759f934ee11d0e6ef85ad
Geoffrey Corey <coreyg@osuosl.org>
2013-12-13 17:02:30 -0800

    Modified it to allow you to specify where the DB is, not install the server itself, and use data bags for database information

--- recipes/source.rb
+++ recipes/source.rb
@@ -52,63 +52,9 @@ node['redmine']['packages'][adapter].each do |pkg|
 end
 case adapter
 when "mysql"
-  include_recipe "mysql::server"
   include_recipe "database::mysql"
 when "postgresql"
-  include_recipe "postgresql::server"
   include_recipe "database::postgresql"
-end
-
-case adapter
-when "mysql"
-  connection_info = {
-    :host => "localhost",
-    :username => 'root',
-    :password => node['mysql']['server_root_password'].empty? ? '' : node['mysql']['server_root_password']
-  }
-when "postgresql"
-  connection_info = {
-    :host => "localhost",
-    :username => 'postgres',
-    :password => node['postgresql']['password']['postgres'].empty? ? '' : node['postgresql']['password']['postgres']
-  }
-end
-
-database node["redmine"]["databases"][environment]["database"] do
-  connection connection_info
-  case adapter
-  when "mysql"
-    provider Chef::Provider::Database::Mysql
-  when "postgresql"
-    provider Chef::Provider::Database::Postgresql
-  end
-  action :create
-end
-
-database_user node["redmine"]["databases"][environment]["username"] do
-  connection connection_info
-  password   node["redmine"]["databases"][environment]["password"]
-  case adapter
-  when "mysql"
-    provider Chef::Provider::Database::MysqlUser
-  when "postgresql"
-    provider Chef::Provider::Database::PostgresqlUser
-  end
-  action :create
-end
-
-database_user node["redmine"]["databases"][environment]["username"] do
-  connection    connection_info
-  database_name node["redmine"]["databases"][environment]["database"]
-  password node["redmine"]["databases"][environment]["password"]
-  case adapter
-  when "mysql"
-    provider Chef::Provider::Database::MysqlUser
-  when "postgresql"
-    provider Chef::Provider::Database::PostgresqlUser
-  end
-  privileges [:all]
-  action :grant
 end
 
 #Setup Apache
@@ -121,7 +67,7 @@ end
 web_app "redmine" do
   docroot        ::File.join(node['redmine']['path'], 'public')
   template       "redmine.conf.erb"
-  server_name    "redmine.#{node['domain']}"
+  server_name    "#{node['domain']}"
   server_aliases [ "redmine", node['hostname'] ]
   rails_env      environment
 end
@@ -155,6 +101,7 @@ else
 end
 
 # deploy the Redmine app
+redmine_db = data_bag_item('redmine', node['redmine']['id'])
 include_recipe "git"
 deploy_revision node['redmine']['deploy_to'] do
   repo     node['redmine']['repo']
@@ -180,8 +127,8 @@ deploy_revision node['redmine']['deploy_to'] do
       group node['apache']['group']
       mode "644"
       variables(
-        :host => 'localhost',
-        :db   => node['redmine']['databases'][environment],
+        :host => redmine_db['host'],
+        :db   => redmine_db,
         :rails_env => environment
       )
     end
