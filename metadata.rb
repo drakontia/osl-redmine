67abdce405619bad03080002ac56b8dfc1324e04
Jordan Evans <jordane@osuosl.org>
2014-04-01 15:31:39 -0700

    sort dependency list, fix FC007

--- metadata.rb
+++ metadata.rb
@@ -9,13 +9,14 @@ recipe "osl-redmine", "Install the Redmine application from the source"
 recipe "osl-redmine::source", "Install the Redmine application from the source"
 recipe "osl-redmine::package", "Install the Redmine application from packages"
 
-depends          'git'
 depends          'apache2'
-depends          'passenger_apache2'
+depends          'apt'
+depends          'database'5
+depends          'git'
 depends          'mysql', '= 3.0.12'
+depends          'passenger_apache2'
 depends          'postgresql'
 depends          'yum'
-depends          'database'
 
 %w{ centos redhat amazon scientific fedora suse }.each do |os|
     supports os
